6cc1ab2446bacaf40063033b53cf495be4ffae84
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2020-10-09 02:29:46 -0300

    Added downloader from youtube using ytdl-core and ffmpeg. highestquality Audio and higestquality Video&Audio + download

--- server.js
+++ server.js
@@ -8,19 +8,14 @@ const fs = require('fs');
 
 const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const ffmpeg = require('fluent-ffmpeg');
+const { nextTick } = require('process');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 // Init
 const app = express();
-app.use(
-  express.static('public'),
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    exposedHeaders: ['Content-Disposition'],
-  })
-);
+app.use(express.static('public'));
+app.use(cors());
 
 var dir = 'public';
 var subDirectory = 'public/uploads';
@@ -48,18 +43,55 @@ app.get('/download', async (req, res) => {
 
     // Get metainfo from video
     ytdl.getInfo(id).then((info) => {
-      const title = info.videoDetails.title;
-      console.log(title);
+      const title = info.videoDetails.title
+        .split(' ')
+        .join('')
+        .replace(/\W/g, '');
 
       if (itag !== 'mp3') {
-        const video = ytdl(url, {
-          filter: 'videoonly',
-          quality: 'highestvideo',
+        const ytdlVideo = fs.createWriteStream(
+          `${subDirectory}/${Date.now()}Video.mp4`
+        );
+        const ytdlAudio = fs.createWriteStream(
+          `${subDirectory}/${Date.now()}Audio.mp4`
+        );
+
+        const output = `${Date.now() + title}`;
+
+        ytdlAudio.on('finish', () => {
+          console.log(`${title} downloaded audio successfully`),
+            ytdl(url, {
+              filter: 'videoonly',
+              quality: 'highestvideo',
+            }).pipe(ytdlVideo);
+          ytdlVideo.on('finish', () => {
+            console.log(`${title} downloaded video successfully`),
+              exec(
+                `ffmpeg -i ${ytdlVideo.path} -i ${ytdlAudio.path} -c:v copy -c:a aac ${subDirectory}/${output}.mp4`,
+                (error) => {
+                  if (error) {
+                    console.log('error: ' + error.message);
+                  } else {
+                    console.log('File has been created');
+                    fs.unlinkSync(ytdlVideo.path);
+                    fs.unlinkSync(ytdlAudio.path);
+                    const ytdlFinal = `${subDirectory}/${output}.mp4`;
+                    res.download(ytdlFinal, (err) => {
+                      if (err) throw err;
+                      console.log("nombre o path del archivo: " + ytdlFinal);
+                      fs.unlinkSync(ytdlFinal);
+                      console.log('termino');
+                    });
+                  }
+                }
+              );
+          });
         });
-        const audio = ytdl(url, {
+
+        ytdl(url, {
           filter: 'audioonly',
           quality: 'highestaudio',
-        });
+        }).pipe(ytdlAudio);
       } else {
         const ytdlVideo = fs.createWriteStream(
           `${subDirectory}/${Date.now()}.mp4`
@@ -69,27 +101,26 @@ app.get('/download', async (req, res) => {
           console.log(`${title} downloaded successfully`);
           var output = `${subDirectory}/${Date.now()}output.mp3`;
 
-          // ffmpeg(ytdlVideo.path).inputOption('-i').format('mp3');
-          // console.log("WOWW")
-
           exec(`ffmpeg -i ${ytdlVideo.path} ${output}`, (error) => {
             if (error) {
               console.log('error: ' + error.message);
             } else {
               console.log('File has been created');
+              res.download(output, (err) => {
+                if (err) throw err;
+                fs.unlinkSync(ytdlVideo.path);
+                fs.unlinkSync(output);
+              });
             }
           });
         });
 
-        const audio = ytdl(url, {
+        ytdl(url, {
           filter: 'audioonly',
           quality: 'highestaudio',
         }).pipe(ytdlVideo);
-        
       }
     });
-
-    // ytdl(url, { filter: (format) => format.container === `${itag}` }).pipe(res);
   } catch (error) {
     console.log(error);
   }
